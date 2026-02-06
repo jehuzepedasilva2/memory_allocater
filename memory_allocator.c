95d77ad9240d7b2d73c328b630c979b44e5db664
Jehu Zepeda-Silva <jehuzepeda-silva@Jehus-MacBook-Pro.local>
2026-02-06 04:14:59 -0800

    added allocater file

---
+++ memory_allocator.c
@@ -0,0 +1,145 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//=================== CONSTANTS ==========================
+int MEMORY_SIZE = 1000;
+
+/*
+=================== TREE DATA STRUCTURE ==================
+This will keep track of the free memory blocks keyed by size [block_size, block_size]:
+        [8, 2]
+        /    \
+    [3, 5]  [10, 10]
+    /   \
+[2, 0] [6, 2]
+For brevity I will keep the tree BST unbalanced, could add it later
+*/
+
+// NOTE TO SELF: If you're going to refrence the same struct within the struct, must add a name
+typedef struct AvailableNode {
+    int key_block_size;
+    int val_block_start;
+    struct AvailableNode* left;
+    struct AvailableNode* right;
+} AvailableNode;
+
+AvailableNode* put_block(AvailableNode* node, int key_size, int val_start);
+AvailableNode* find_block(AvailableNode* node, int key_size, int* min_diff, AvailableNode* result);
+AvailableNode* delete_block(AvailableNode* node, int key_size, int val_start);
+AvailableNode* get_inorder_succesor(AvailableNode* node);
+AvailableNode* balance_tree(AvailableNode* node);
+AvailableNode** level_order_traversal(AvailableNode* node, int* out_length, int max_nodes);
+
+AvailableNode* put_block(AvailableNode* node, int key_size, int val_start) {
+    if (node == NULL) {
+        AvailableNode* new_node = malloc(sizeof(AvailableNode));
+        if (new_node == NULL) {
+            return NULL;
+        }
+        new_node->key_block_size = key_size;
+        new_node->val_block_start = val_start;
+        // clean up the allocated memory for key_val pair
+        return new_node;
+    }
+    if (key_size <= node->key_block_size) {
+        node->left = put_block(node->left, key_size, val_start);
+    } else {
+        node->right = put_block(node->right, key_size, val_start);
+    }
+    return node;
+}
+
+AvailableNode* find_block(AvailableNode* node, int key_size, int* min_diff, AvailableNode* result) {
+    // if we found nothing, means there are no free blocks >= size we want
+    if (node == NULL) {
+        return result;
+    }
+    // we search right if the current node size is smaller
+    if (node->key_block_size < key_size) {
+        return find_block(node->right, key_size, min_diff, result);
+    // search left otherwise, and find teh closest thing
+    } else if (node->key_block_size >= key_size) {
+        int current_diff = node->key_block_size - key_size;
+        if (current_diff < *min_diff) {
+            *min_diff = current_diff;
+            result = node;
+        }
+        return find_block(node->left, key_size, min_diff, result);
+    } 
+}
+
+/*
+               8
+            /    \
+          3      10
+        /   \      \    
+       2     6     13
+    We have a few cases:
+    1. The tree is empty or the node doesnt exist;
+        - In thi case we just return null
+    2. We are deleting a leaf
+        - return null to the parent pointer
+    3. Node has only 1 child
+        - Replace parent node with its child
+    4. Node has two children (trickiest)
+        - Replace node with inorder successor, and delete that node recursively
+*/
+AvailableNode* delete_block(AvailableNode* node, int key_size, int val_start) {
+    // case 1:
+    if (node == NULL) {
+        return NULL;
+    }
+    // once we find the node:
+    if (node->key_block_size == key_size && node->val_block_start == val_start) {
+        // case 2:
+        if (node->left == NULL && node->right == NULL) {
+            // free memory and return null
+            free(node);
+            return NULL;
+        // case 3:
+        } else if (node->left == NULL) {
+            AvailableNode* right_node = node->right;
+            free(node);
+            return right_node;
+        } else if (node->right == NULL) {
+            AvailableNode* left_node = node->left;
+            free(node);
+            return left_node;
+        // case 4
+        } else {
+            // find the inorder succesor, and replace with this one
+            // inorder succesor is the leftmost child of the right subtree
+            AvailableNode* inorder_succesor = get_inorder_succesor(node->right);
+            node->key_block_size = inorder_succesor->key_block_size;
+            node->val_block_start = inorder_succesor->val_block_start;
+            node->right = delete_block(node->right, inorder_succesor->key_block_size, inorder_succesor->val_block_start);
+        }
+
+    // if current node is smaller than size we want go right
+    } else if (node->key_block_size < key_size) {
+        node->right = delete_block(node->right, key_size, val_start);
+    // otherwise search for it in the right tree
+    } else {
+        node->left = delete_block(node->left, key_size, val_start);
+    }
+    return node;
+}
+
+// go left all way
+AvailableNode* get_inorder_succesor(AvailableNode* node) {
+    if (node->left == NULL) {
+        return node;
+    }
+    return get_inorder_succesor(node->left);
+}
+
+/*
+=================== MAIN PROGRAM =======================
+*/
+ AvailableNode* free_blocks = NULL;
+
+int main() {
+    free_blocks = put_block(free_blocks, MEMORY_SIZE, 0);
+    return 0;
+}
